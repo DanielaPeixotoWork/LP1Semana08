42a1b917dac6b8e955c4ab876ab26de9c2ac53f9
DanielaPeixotoWork <danielapeixoto.work@gmail.com>
2024-04-24 20:30:40 +0100

    First commit

---
+++ AnimalKindom/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AnimalKindom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
